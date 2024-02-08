7ac1c4891e86f5ce12bff3d48b654d2ce211c01c
sanjeev929 <sanjeevsanju929@gmail.com>
2024-02-08 08:00:55 +0530

    add api

--- projectcertificate/certificateapp/views.py
+++ projectcertificate/certificateapp/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def registartion(request):
     return render(request,'registration.html')
 def home(request):
-    name = request.POST["name"]
-    email = request.POST["email"]
-    phone = request.POST["phone"]
-    print(name,email,phone)
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        data = {
+            "name": name,
+            "email": email,
+            "phone": phone
+        }
+        fastapi_url = "http://192.168.143.87:8001/submit-data/"
+        response = requests.post(fastapi_url, json=data)
+        if response.status_code == 200:
+            response_data = response.json()
+            message = response_data.get("message")
+            context={
+                "name":name,
+                "email":email
+            }
+            return render(request, 'home.html',context)
+        else:
+                context={
+                "name":None,
+                "email":None
+            }
+        return render(request, 'home.html',context)
     context={
-        "name":name,
-        "email":email
-    }
-    return render(request,'home.html',context)
+                "name":None,
+                "email":None
+            }    
+    return render(request, 'home.html',context)
